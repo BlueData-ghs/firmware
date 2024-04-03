17e331a99499d2ffe449f2e759ad215638c6f896
Matt Gleich <git@mattglei.ch>
2024-04-03 16:46:42 -0400

    feat: Gavin's Thermometer code
    Co-authored-by: Gavin Palmer <gmpalmer124@gmail.com>
    
    Signed-off-by: Matt Gleich <git@mattglei.ch>

---
+++ arduino/Thermometer.ino
@@ -0,0 +1,22 @@
+const int tempPin = 0; // analog pin 0
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int tempReading = analogRead(tempPin);
+
+  double tempK = log(10000.0 * ((1024.0 / tempReading - 1)));
+  tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK * tempK )) * tempK ); // Temp Kelvin
+  float tempC = tempK - 273.15; // Convert Kelvin to Celcius
+
+  // Display Temperature in C
+  Serial.print("Temp: ");
+  Serial.print(tempC);
+  Serial.println("C");
+
+  delay(1000);
+}
