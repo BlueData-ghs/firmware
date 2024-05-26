2ade6b58196ffa31987e7233b4cad912f9454788
Matt Gleich <git@mattglei.ch>
2024-05-26 14:36:19 -0400

    feat: write data to CSV file
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- bluedata/main.py
+++ bluedata/main.py
@@ -1,8 +1,10 @@
 from loguru import logger
+import db
 
 
 def main() -> None:
     logger.info("Hello world!")
+    db.write_data(temp=50.3, light=0.80, salinity=0.30)
 
 
 if __name__ == "__main__":
