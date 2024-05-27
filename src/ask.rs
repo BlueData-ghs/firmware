07a69f74acb993b422e1169f27893cbfd257d02a
Matt Gleich <git@mattglei.ch>
2024-05-27 15:57:48 -0400

    feat: ask for team number
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/ask.rs
+++ src/ask.rs
@@ -3,11 +3,24 @@ use std::{io, process::exit};
 use anyhow::{Context, Result};
 use color_print::cprintln;
 
+/// Ask the user the ream number that they are apart of
+pub fn team_number() -> Result<u32> {
+    println!("\nBefore we get started what team number are you?");
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .context("Failed to ask for team number by reading line from buffer")?;
+    Ok(input
+        .trim()
+        .parse::<u32>()
+        .context("Failed to parse number from input")?)
+}
+
 /// Ask the user the depth of the current measurements
 ///
 /// If the user is done measuring, they will input "done" and the program will exit with a status code of 0
 pub fn depth() -> Result<u32> {
-    println!("\nCurrent depth in meters: ");
+    println!("\nCurrent depth in meters?");
     let mut input = String::new();
     io::stdin()
         .read_line(&mut input)
--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,16 @@ mod db;
 fn main() {
     db::verify_drive().expect("Failed to verify drive");
     print_header();
+    let team_number = ask::team_number().expect("Failed to ask user for team number");
+    dbg!(team_number);
+
     let depth = ask::depth().expect("Failed to ask for depth");
     dbg!(depth);
 }
 
 fn print_header() {
     cprintln!("\nWelcome to <blue>BlueData</blue>!");
-    println!("\nThis program will record the following information and store it in a CSV (comma separated value) file for your team:");
+    println!("\nThis program will record the following information and store it in a CSV (comma separated value) file for your team. This can then be loaded into a spreadsheet program such as Microsoft Excel.");
     cprintln!("\t1. <yellow>Temperature</yellow> (°C)");
     cprintln!("\t2. <magenta>Light</magenta> (% from surface)");
     cprintln!("\t3. <cyan>Salinity</cyan> (g/L)");
